447e71fa1c3efe4b52955ca812209c24737fc732
TRUONG <kdt@lem.com>
2021-03-08 15:52:59 +0100

    add PLIN Object for test fixture

--- conftest.py
+++ conftest.py
@@ -1,8 +1,12 @@
 import sys
-sys.path.append('C:\\Users\\mlo\\Volvo\\pythontestbench\\Libs')
+import os
+#sys.path.append('C:\\Users\\mlo\\Volvo\\pythontestbench\\Libs')
+sys.path.append('C:\\Projet\\Python_bench_Git_Test\\pythontestbench\\Libs')
 import Libs.NI_8012 as NI_8012
 import Libs.canStatistics as cs
 from pytest import fixture
+from VolvoLinCom import Lin_Peak_For_Volvo
+import time
 
 
 @fixture(scope="session")
@@ -27,3 +31,19 @@ def can_interface():
     can = cs.canStatistics(bitrate=250000)
     yield can
     can.bus.shutdown()
+
+@fixture(scope="session")
+def app_lin_com():
+    linvolvo = Lin_Peak_For_Volvo()
+    linvolvo.connect("appLin_Volvo", 1, 1)
+    time.sleep(1)
+    linvolvo.disconnect()
+    time.sleep(1)
+    linvolvo.connect("appLin_Volvo", 1, 1)
+    linvolvo.set_global_frame_table()
+    time.sleep(1)
+    linvolvo.delete_schedule_table(0)
+    time.sleep(1)
+    linvolvo.set_schedule_table(0)
+    yield linvolvo
+    linvolvo.disconnect()
