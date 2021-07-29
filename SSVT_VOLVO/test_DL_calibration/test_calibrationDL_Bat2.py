f23d987fa3f30dd06e6a06bc974e14c30adfc798
Houssam <hmy@lem.com>
2021-07-29 14:54:56 +0200

    test

--- SSVT_VOLVO/test_DL_calibration/test_calibrationDL_Bat2.py
+++ SSVT_VOLVO/test_DL_calibration/test_calibrationDL_Bat2.py
@@ -2,7 +2,7 @@ import Libs.NI_8012 as NI_8012
 from pytest import mark
 import time
 import subprocess
-from subprocess import Popen,PIPE,STDOUT
+from subprocess import  Popen,PIPE,STDOUT
 from functools import wraps
 from pytest_html import extras
 from testbenchselect import *
